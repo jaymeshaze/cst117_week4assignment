997eebd936338af7fe063f480ab40474ca46b87c
jaymeshaze <jaymeshaze@outlook.com>
2017-01-23 12:35:26 -0700

    Initial Upload

---
+++ Exercise5_10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Revise the BankAccount class of Example 5.1 to overload the Withdraw method.
+//Include a withdraw method with no parameters, which will withdraw $40 if available,
+//and display a message otherwise.Add tests of this new method to Example 5.2.
+
+namespace Exercise5_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
