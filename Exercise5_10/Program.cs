fc6aeaf60fbdc4c8194608ef46878d733c6da147
jaymeshaze <jaymeshaze@outlook.com>
2017-01-29 14:05:00 -0700

    Updated exercise 5.10

--- Exercise5_10/Program.cs
+++ Exercise5_10/Program.cs
@@ -14,6 +14,82 @@ namespace Exercise5_10
     {
         static void Main(string[] args)
         {
+            BankAccount myAccount = new BankAccount(25.00);
+
+            Console.WriteLine("My balance = {0:C}", myAccount.GetBalance());
+            myAccount.Deposit(700.00);
+            Console.WriteLine("My balance = {0:C}", myAccount.GetBalance());
+
+            if (myAccount.Withdraw(300.00) < 0)
+                Console.WriteLine("Insufficient funds");
+
+            Console.WriteLine("My balance = {0:C}", myAccount.GetBalance());
+
+            if (myAccount.Withdraw(450.00) < 0)
+                Console.WriteLine("Insufficient funds");
+            Console.WriteLine("My balance = {0:C}", myAccount.GetBalance());
+
+            BankAccount yourAccount = new BankAccount();
+            yourAccount.Deposit(1234.56);
+            Console.WriteLine("Your balance = {0:C}", yourAccount.GetBalance());
+
+            BankAccount krisTestAccount = new BankAccount();
+            Console.WriteLine(krisTestAccount.Withdraw());
+
         }
+
+        /* Declares a BankAccount class with an account balance,
+        * two constructors, and GetBalance, Deposit, and
+        * Withdraw operations.
+        */
+        public class BankAccount
+        {
+            private double balance;
+                                    
+            // Creates a Bank Account with a balance of 0
+            public BankAccount()
+            {
+                balance = 0;
+            }
+            // Creates a Bank Account with a balance of initialAmount
+            public BankAccount(double initialAmount)
+            {
+                balance = initialAmount;
+            }
+            // Returns the balance
+            public double GetBalance()
+            {
+                return balance;
+            }
+            // Increases balance by amount
+            public void Deposit(double amount)
+            {
+                balance += amount;
+            }
+            // Reduces balance by amount, if possible
+            public double Withdraw(double amount)
+            {
+                if (balance >= amount)
+                {
+                    balance -= amount;
+                    return balance;
+                }
+                else
+                    return -1.0;
+            }
+            //Reduces balance by $40, if possible
+            public String Withdraw()
+            {
+                if (balance >= 40)
+                {
+                    balance -= 40;
+                    return "Successfully withdrew $40.00";
+                }
+                else
+                    return "Insufficient funds";
+            }
+        }
+■
+
     }
 }
